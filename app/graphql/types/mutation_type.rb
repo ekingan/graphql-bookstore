421e217da450af44c73ee6fcf0b7becec5573058
Emily Kingan <ekingan@Emilys-MacBook-Pro-2.local>
2019-04-29 10:45:08 -0700

    Initial commit

---
+++ app/graphql/types/mutation_type.rb
@@ -0,0 +1,7 @@
+module Types
+  class MutationType < Types::BaseObject
+    field :create_author, mutation: Mutations::CreateAuthor
+    field :update_author, mutation: Mutations::UpdateAuthor
+    field :delete_author, mutation: Mutations::DeleteAuthor
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+10.times do
+  author = Author.create name: Faker::Book.author, age: Faker::Number.between(12, 100)
+
+  5.times do
+    author.books.create title: Faker::Book.title, genre: Faker::Book.genre
+  end
+end
+
+puts "seeded yo"
